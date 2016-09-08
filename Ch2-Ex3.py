ff3aa28b45000186175ecb6aafc49834e2611259
dhruvgohel <ddg20@pitt.edu>
2016-09-08 13:56:48 -0400

    final program

---
+++ Ch2-Ex3.py
@@ -0,0 +1,15 @@
+#name:          Dhruv Gohel
+#email:         ddg20@pitt.edu
+#date:          09/08/2016
+#class:         CS0008-f2016
+#instructor:    Max Novelli
+#
+#description:   Introduction to programming with Python, Chapter 2, Exercise 3
+#
+#Notes:
+
+acre = 4046.8564224
+totalLand = input("Enter the total square meters of land: ")
+totalAcres = 0
+totalAcres = totalLand/acre
+print('The total number of acres is:', totalAcres, 'm^2')
