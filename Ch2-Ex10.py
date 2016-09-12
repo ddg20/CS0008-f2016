d5de692400a34c178c8fdefc575a3e3b84910124
dhruvgohel <ddg20@pitt.edu>
2016-09-12 12:24:56 -0400

    ch2-ex10 final (new line and commas do not seem to be working

--- Ch2-Ex10.py
+++ Ch2-Ex10.py
@@ -4,7 +4,18 @@
 #class:         CS0008-f2016
 #instructor:    Max Novelli
 #
-#description:   Introduction to programming with Python, Chapter 2, Exercise 7
+#description:   Introduction to programming with Python, Chapter 2, Exercise 10
 #
-#Notes:
+#Notes: newline is not working
 
+sugar = 300 / 48
+butter = 240 / 48
+flour = 330 / 48
+
+cookies = int(input("How many cookies would you like to make?"))
+
+totalSugar = cookies * sugar
+totalButter = cookies * butter
+totalFlour = cookies * flour
+
+print('You need\n', totalSugar, 'grams of sugar\n', totalButter, 'grams of butter\n', totalFlour, 'grams of flour')
