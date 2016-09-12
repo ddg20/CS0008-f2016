877a2e6142c69d2ba95e1dfec2e2005cfb25b8fd
dhruvgohel <ddg20@pitt.edu>
2016-09-12 12:08:23 -0400

    ch2-ex9 final

---
+++ Ch2-Ex9.py
@@ -0,0 +1,16 @@
+#name:          Dhruv Gohel
+#email:         ddg20@pitt.edu
+#date:          09/08/2016
+#class:         CS0008-f2016
+#instructor:    Max Novelli
+#
+#description:   Introduction to programming with Python, Chapter 2, Exercise 7
+#
+#Notes:
+
+fahrenheit = int(input("Enter a temperature in degrees Fahrenheit"))
+
+celsius = 5 * (fahrenheit - 32) / 9
+
+
+print("The temperature in degrees Celsius is", celsius)
