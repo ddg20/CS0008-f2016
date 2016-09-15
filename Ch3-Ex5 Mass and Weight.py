d69d2ce0c331901d20e68a0d6d2a168abd5ab7e5
dhruvgohel <ddg20@pitt.edu>
2016-09-14 20:30:50 -0400

    final mass and weight program

---
+++ Ch3-Ex5 Mass and Weight.py
@@ -0,0 +1,20 @@
+#name:          Dhruv Gohel
+#email:         ddg20@pitt.edu
+#date:          09/08/2016
+#class:         CS0008-f2016
+#instructor:    Max Novelli
+#
+#description:   Introduction to programming with Python, Chapter 3, Exercise 4
+#
+#Notes:
+
+mass = (int(input("Enter an object's mass: ")))
+
+weight = mass * 9.8
+
+if weight > 500:
+    print("\nThe object is too heavy.")
+elif weight < 100:
+    print("\nThe object is too light")
+else:
+    print("\nThe object's weight is: %s") %weight
